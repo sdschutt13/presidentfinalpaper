537bc119d1d6895daac2a2aeb429ca5c053ac897
sdschutt13 <sdschutt@wisc.edu>
2019-05-16 17:37:47 -0500

    graphics addition

--- edits/table2fixesreal.R
+++ edits/table2fixesreal.R
@@ -508,33 +508,276 @@ pred.alt3def.hand<-augment(alt3def.hand,
 ## GRAPHICS ##
 ##############
 
+#####
+## SET UP FOR GRAPHICS ##
+labelpot <- c("-2" = "Strongly Disagree", "-1" = "Disagree", "0" = "Neither", "1" = "Agree", "2" = "Strongly Agree")
+
+#####
+
 #########
 ## POT ##
 #########
 #####
-## ORIGINAL PREDICTED POT ##
+## ALTERNATIVE 1 PREDICTED POT CANDIDATE ##
 
-pred.alt1pot.cand %>% 
+pred.alt1pot.cand %>% drop_na() %>%
   ggplot() + 
     aes(x = pid3, y = .fitted, color = pid3) + 
      geom_pointrange(aes(ymin = .fitted - 1.96*.se.fit,
                       ymax = .fitted + 1.96*.se.fit) )  + 
       coord_flip() +
-      facet_wrap("pot.attitudes", ncol = 1) +
-      labs(y = "Probability", x = "") + 
-      scale_color_discrete() + 
-      theme(axis.title.y = element_blank(),
+      facet_wrap("pot.attitudes", ncol = 1, labeller = labeller("pot.attitudes" = labelpot)) +
+      labs(y = "Probability of Support for Candidate", x = "Support for Marijuana Legalization") + 
+      labs(color ="Party ID")+
+      labs(title = "Unilateral Action Use for Pot\n Support of Candidate", subtitle = "Alternative Model 1 Candidate")+
+      scale_color_manual(limits= c("dem", "ind", "gop"), values = c("dodgerblue3", "purple3", "red4"), labels = c("Democrats", "Independents", "Republicans")) +
+      scale_x_discrete(limits= c("gop", "ind", "dem"),
+                       breaks = c("0", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1")) +
+      theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 15, face="bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 8))
+
+#####
+## ALTERNATIVE 1 PREDICTED POT HANDLING ##
+
+pred.alt1pot.hand %>% drop_na() %>%
+  ggplot() + 
+  aes(x = pid3, y = .fitted, color = pid3) + 
+  geom_pointrange(aes(ymin = .fitted - 1.96*.se.fit,
+                      ymax = .fitted + 1.96*.se.fit) )  + 
+  coord_flip() +
+  facet_wrap("pot.attitudes", ncol = 1, labeller = labeller("pot.attitudes" = labelpot)) +
+  labs(y = "Probability of Support for Handling", x = "Support for Marijuana Legalization") + 
+  labs(color ="Party ID")+
+  labs(title = "Unilateral Action Use for Pot\n Support of Candidate's Handling", subtitle = "Alternative Model 1 Handling")+
+  scale_color_manual(limits= c("dem", "ind", "gop"), values = c("dodgerblue3", "purple3", "red4"), labels = c("Democrats", "Independents", "Republicans")) +
+  scale_x_discrete(limits= c("gop", "ind", "dem"),
+                   breaks = c("0", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1")) +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 15, face="bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 8))
+
+#####
+## ALTERNATIVE 2 PREDICTED POT CANDIDATE ##
+
+pred.alt2pot.cand %>% drop_na() %>%
+  ggplot() + 
+  aes(x = pid3, y = .fitted, color = pid3) + 
+  geom_pointrange(aes(ymin = .fitted - 1.96*.se.fit,
+                      ymax = .fitted + 1.96*.se.fit) )  + 
+  coord_flip() +
+  facet_wrap("pot.attitudes", ncol = 1, labeller = labeller("pot.attitudes" = labelpot)) +
+  labs(y = "Probability of Support for Candidate", x = "Support for Marijuana Legalization") + 
+  labs(color ="Party ID")+
+  labs(title = "Unilateral Action Use for Pot\n Support of Candidate", subtitle = "Alternative Model 2 Candidate")+
+  scale_color_manual(limits= c("dem", "ind", "gop"), values = c("dodgerblue3", "purple3", "red4"), labels = c("Democrats", "Independents", "Republicans")) +
+  scale_x_discrete(limits= c("gop", "ind", "dem"),
+                   breaks = c("0", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1")) +
+  theme(axis.title.y = element_blank(),
         axis.text.y = element_blank(),
-        axis.ticks.y = element_blank())
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 15, face="bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 8))
+
+#####
+## ALTERNATIVE 2 PREDICTED POT HANDLING ##
+
+pred.alt2pot.hand %>% drop_na() %>%
+  ggplot() + 
+  aes(x = pid3, y = .fitted, color = pid3) + 
+  geom_pointrange(aes(ymin = .fitted - 1.96*.se.fit,
+                      ymax = .fitted + 1.96*.se.fit) )  + 
+  coord_flip() +
+  facet_wrap("pot.attitudes", ncol = 1, labeller = labeller("pot.attitudes" = labelpot)) +
+  labs(y = "Probability of Support for Handling", x = "Support for Marijuana Legalization") + 
+  labs(color ="Party ID")+
+  labs(title = "Unilateral Action Use for Pot\n Support of Candidate's Handling", subtitle = "Alternative Model 2 Handling")+
+  scale_color_manual(limits= c("dem", "ind", "gop"), values = c("dodgerblue3", "purple3", "red4"), labels = c("Democrats", "Independents", "Republicans")) +
+  scale_x_discrete(limits= c("gop", "ind", "dem"),
+                   breaks = c("0", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1")) +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 15, face="bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 8))
+
+#####
+## ALTERNATIVE 3 PREDICTED POT CANDIDATE ##
+
+pred.alt3pot.cand %>% drop_na() %>%
+  ggplot() + 
+  aes(x = pid3, y = .fitted, color = pid3) + 
+  geom_pointrange(aes(ymin = .fitted - 1.96*.se.fit,
+                      ymax = .fitted + 1.96*.se.fit) )  + 
+  coord_flip() +
+  facet_wrap("pot.attitudes", ncol = 1, labeller = labeller("pot.attitudes" = labelpot)) +
+  labs(y = "Probability of Support for Candidate", x = "Support for Marijuana Legalization") + 
+  labs(color ="Party ID")+
+  labs(title = "Unilateral Action Use for Pot\n Support of Candidate", subtitle = "Alternative Model 3 Candidate")+
+  scale_color_manual(limits= c("dem", "ind", "gop"), values = c("dodgerblue3", "purple3", "red4"), labels = c("Democrats", "Independents", "Republicans")) +
+  scale_x_discrete(limits= c("gop", "ind", "dem"),
+                   breaks = c("0", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1")) +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 15, face="bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 8))
+
+#####
+## ALTERNATIVE 3 PREDICTED POT HANDLING ##
+
+pred.alt3pot.hand %>% drop_na() %>%
+  ggplot() + 
+  aes(x = pid3, y = .fitted, color = pid3) + 
+  geom_pointrange(aes(ymin = .fitted - 1.96*.se.fit,
+                      ymax = .fitted + 1.96*.se.fit) )  + 
+  coord_flip() +
+  facet_wrap("pot.attitudes", ncol = 1, labeller = labeller("pot.attitudes" = labelpot)) +
+  labs(y = "Probability of Support for Handling", x = "Support for Marijuana Legalization") + 
+  labs(color ="Party ID")+
+  labs(title = "Unilateral Action Use for Pot\n Support of Candidate's Handling", subtitle = "Alternative Model 3 Handling")+
+  scale_color_manual(limits= c("dem", "ind", "gop"), values = c("dodgerblue3", "purple3", "red4"), labels = c("Democrats", "Independents", "Republicans")) +
+  scale_x_discrete(limits= c("gop", "ind", "dem"),
+                   breaks = c("0", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1")) +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 15, face="bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 8))
 
 
+#####
 
+#########
+## TAX ##
+#########
+#####
+## ALTERNATIVE 1 PREDICTED TAX CANDIDATE ##
 
+pred.alt1tax.cand %>% drop_na() %>%
+  ggplot() + 
+  aes(x = pid3, y = .fitted, color = pid3) + 
+  geom_pointrange(aes(ymin = .fitted - 1.96*.se.fit,
+                      ymax = .fitted + 1.96*.se.fit) )  + 
+  coord_flip() +
+  facet_wrap("tax.attitudes", ncol = 1, labeller = labeller("tax.attitudes" = labelpot)) +
+  labs(y = "Probability of Support for Candidate", x = "Support for Taxing Corporations") + 
+  labs(color ="Party ID")+
+  labs(title = "Unilateral Action Use for Tax\n Support of Candidate", subtitle = "Alternative Model 1 Taxes - Candidate")+
+  scale_color_manual(limits= c("dem", "ind", "gop"), values = c("dodgerblue3", "purple3", "red4"), labels = c("Democrats", "Independents", "Republicans")) +
+  scale_x_discrete(limits= c("gop", "ind", "dem"),
+                   breaks = c("0", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1")) +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 15, face="bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 8))
 
+#####
+## ALTERNATIVE 1 PREDICTED TAX HANDLING ##
 
+pred.alt1tax.hand %>% drop_na() %>%
+  ggplot() + 
+  aes(x = pid3, y = .fitted, color = pid3) + 
+  geom_pointrange(aes(ymin = .fitted - 1.96*.se.fit,
+                      ymax = .fitted + 1.96*.se.fit) )  + 
+  coord_flip() +
+  facet_wrap("tax.attitudes", ncol = 1, labeller = labeller("tax.attitudes" = labelpot)) +
+  labs(y = "Probability of Support for Handling", x = "Support for Taxing Corporations") + 
+  labs(color ="Party ID")+
+  labs(title = "Unilateral Action Use for Tax\n Support of Candidate's Handling", subtitle = "Alternative Model 1 Taxes - Handling")+
+  scale_color_manual(limits= c("dem", "ind", "gop"), values = c("dodgerblue3", "purple3", "red4"), labels = c("Democrats", "Independents", "Republicans")) +
+  scale_x_discrete(limits= c("gop", "ind", "dem"),
+                   breaks = c("0", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1")) +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 15, face="bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 8))
 
+#####
+## ALTERNATIVE 2 PREDICTED TAX CANDIDATE ##
 
+pred.alt2tax.cand %>% drop_na() %>%
+  ggplot() + 
+  aes(x = pid3, y = .fitted, color = pid3) + 
+  geom_pointrange(aes(ymin = .fitted - 1.96*.se.fit,
+                      ymax = .fitted + 1.96*.se.fit) )  + 
+  coord_flip() +
+  facet_wrap("tax.attitudes", ncol = 1, labeller = labeller("tax.attitudes" = labelpot)) +
+  labs(y = "Probability of Support for Candidate", x = "Support for Taxing Corporations") + 
+  labs(color ="Party ID")+
+  labs(title = "Unilateral Action Use for Tax\n Support of Candidate", subtitle = "Alternative Model 2 Taxes - Candidate")+
+  scale_color_manual(limits= c("dem", "ind", "gop"), values = c("dodgerblue3", "purple3", "red4"), labels = c("Democrats", "Independents", "Republicans")) +
+  scale_x_discrete(limits= c("gop", "ind", "dem"),
+                   breaks = c("0", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1")) +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 15, face="bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 8))
 
+#####
+## ALTERNATIVE 2 PREDICTED TAX HANDLING ##
+
+pred.alt2tax.hand %>% drop_na() %>%
+  ggplot() + 
+  aes(x = pid3, y = .fitted, color = pid3) + 
+  geom_pointrange(aes(ymin = .fitted - 1.96*.se.fit,
+                      ymax = .fitted + 1.96*.se.fit) )  + 
+  coord_flip() +
+  facet_wrap("tax.attitudes", ncol = 1, labeller = labeller("tax.attitudes" = labelpot)) +
+  labs(y = "Probability of Support for Handling", x = "Support for Taxing Corporations") + 
+  labs(color ="Party ID")+
+  labs(title = "Unilateral Action Use for Tax\n Support of Candidate's Handling", subtitle = "Alternative Model 2 Taxes - Handling")+
+  scale_color_manual(limits= c("dem", "ind", "gop"), values = c("dodgerblue3", "purple3", "red4"), labels = c("Democrats", "Independents", "Republicans")) +
+  scale_x_discrete(limits= c("gop", "ind", "dem"),
+                   breaks = c("0", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1")) +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 15, face="bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 8))
+
+#####
+## ALTERNATIVE 3 PREDICTED TAX CANDIDATE ##
+
+pred.alt3tax.cand %>% drop_na() %>%
+  ggplot() + 
+  aes(x = pid3, y = .fitted, color = pid3) + 
+  geom_pointrange(aes(ymin = .fitted - 1.96*.se.fit,
+                      ymax = .fitted + 1.96*.se.fit) )  + 
+  coord_flip() +
+  facet_wrap("tax.attitudes", ncol = 1, labeller = labeller("tax.attitudes" = labelpot)) +
+  labs(y = "Probability of Support for Candidate", x = "Support for Taxing Corporations") + 
+  labs(color ="Party ID")+
+  labs(title = "Unilateral Action Use for Tax\n Support of Candidate", subtitle = "Alternative Model 3 Taxes - Candidate")+
+  scale_color_manual(limits= c("dem", "ind", "gop"), values = c("dodgerblue3", "purple3", "red4"), labels = c("Democrats", "Independents", "Republicans")) +
+  scale_x_discrete(limits= c("gop", "ind", "dem"),
+                   breaks = c("0", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1")) +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 15, face="bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 8))
+
+#####
+## ALTERNATIVE 3 PREDICTED TAX HANDLING ##
+
+pred.alt3tax.hand %>% drop_na() %>%
+  ggplot() + 
+  aes(x = pid3, y = .fitted, color = pid3) + 
+  geom_pointrange(aes(ymin = .fitted - 1.96*.se.fit,
+                      ymax = .fitted + 1.96*.se.fit) )  + 
+  coord_flip() +
+  facet_wrap("pot.attitudes", ncol = 1, labeller = labeller("pot.attitudes" = labelpot)) +
+  labs(y = "Probability of Support for Handling", x = "Support for Taxing Corporations") + 
+  labs(color ="Party ID")+
+  labs(title = "Unilateral Action Use for Tax\n Support of Candidate's Handling", subtitle = "Alternative Model 3 Taxes - Handling")+
+  scale_color_manual(limits= c("dem", "ind", "gop"), values = c("dodgerblue3", "purple3", "red4"), labels = c("Democrats", "Independents", "Republicans")) +
+  scale_x_discrete(limits= c("gop", "ind", "dem"),
+                   breaks = c("0", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1")) +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(size = 15, face="bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 8))
+
+#####
 
 
 
@@ -690,3 +933,9 @@ table3 <-
     custom.col.pos = 5,
     file = "table3.html"
   )
+
+
+
+
+
+
